caae79f5bb8c34db77f50dd2207f64312912fa57
Niome <athyst7@gmail.com>
2024-01-20 16:01:28 -0800

    backup

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,33 @@
 import {
   E_SDK_EVENT,
-  getVenue,
   Mappedin,
-  MappedinPolygon,
   MapView,
   OfflineSearch,
   showVenue,
-  TGetVenueOptions,
-  TMappedinOfflineSearchResult
+  TGetVenueMakerOptions,
+  TMappedinOfflineSearchResult,
+  getVenueMaker,
+  MappedinLocation
 } from "@mappedin/mappedin-js";
 import "@mappedin/mappedin-js/lib/mappedin.css";
-import { augmentedPolygonThings } from "./defaultThings";
 import productData from "../public/products.json";
 
-// See Trial API key Terms and Conditions
-// https://developer.mappedin.com/guides/api-keys
-const options: TGetVenueOptions = {
-  venue: "mappedin-demo-retail-2",
-  clientId: "5eab30aa91b055001a68e996",
-  clientSecret: "RJyRXKcryCMy4erZqqCbuB1NbR66QTGNXVE0x3Pg6oCIlUR1",
-  things: augmentedPolygonThings // ensures polygon name is fetched
-};
+const options = {
+  mapId: process.env.REACT_APP_MAP_ID,
+  key: process.env.REACT_APP_KEY,
+  secret: process.env.REACT_APP_SECRET,
+} as TGetVenueMakerOptions;
+
 
 let venue: Mappedin;
 let mapView: MapView;
 let search: OfflineSearch;
 
-const resultsElement = document.getElementById("search-results")!;
-const resultsListElement = document.getElementById("search-results-list")!;
-const searchElement = document.getElementById("search")!;
+const resultsElement = document.getElementById("search-results") as HTMLDivElement;
+const resultsListElement = document.getElementById("search-results-list")! as HTMLUListElement;
+const searchElement = document.getElementById("search")! as HTMLInputElement;
 
-function navigateTo(to: MappedinPolygon) {
+function navigateTo(to: MappedinLocation) {
   if (!mapView) return;
   const from = venue.locations.find((l) => l.name == "Entrance")!;
   const directions = from.directionsTo(to);
@@ -44,13 +41,14 @@ function navigateTo(to: MappedinPolygon) {
 
 interface Product {
   name: string;
-  description: string;
-  price: number;
-  polygonName: string;
+  description: string | null;
+  price: number | string | null;
+  location: string;
 }
 
+
 async function init() {
-  venue = await getVenue(options);
+  venue = await getVenueMaker(options);
   search = new OfflineSearch(venue);
   mapView = await showVenue(document.getElementById("map")!, venue);
 
@@ -59,7 +57,7 @@ async function init() {
   });
 
   productData.forEach((product: Product) => {
-    if (product.polygonName) {
+    if (product.location) {
       search.addQuery({
         query: product.name,
         object: { product }
@@ -68,7 +66,7 @@ async function init() {
   });
 
   searchElement.oninput = async (event: Event) => {
-    const query = event.target.value;
+    const query = (event.target as HTMLInputElement).value;
 
     if (query.length < 2) {
       resultsElement.style.display = "none";
@@ -86,31 +84,32 @@ async function init() {
     }
 
     productResults.forEach((result: TMappedinOfflineSearchResult) => {
-      const product = result.object.product as Product;
+      if ("product" in result.object) {
+        const product = result.object.product as Product;
 
-      const resultElement = document.createElement("li");
-      resultElement.innerText = product.name;
-      resultElement.dataset.id = product.polygonName;
-      resultsListElement.appendChild(resultElement);
+        const resultElement = document.createElement("li");
+        resultElement.innerText = product.name;
+        resultElement.dataset.id = product.location;
+        resultsListElement.appendChild(resultElement);
 
-      resultElement.addEventListener("click", (event: MouseEvent) => {
-        mapView.Journey.clear();
-        const id = (event.target as HTMLLIElement).dataset.id;
-        const destination = venue.polygons.find((p) => p.name === id)!;
-        navigateTo(destination);
-        resultsElement.style.display = "none";
-        searchElement.value = product.name;
-      });
+        resultElement.addEventListener("click", (event: MouseEvent) => {
+          mapView.Journey.clear();
+          const id = (event.target as HTMLLIElement).dataset.id;
+          const destination = venue.locations.find((p) => p.name === product.location)!;
+          navigateTo(destination);
+          resultsElement.style.display = "none";
+          searchElement.value = product.name;
+        });
+      }
     });
 
     resultsElement.style.display = "block";
   };
 
-  // initial sample product search
-  const product = productData[1]; // Pasta Sauce
-  const destination = venue.polygons.find(
-    (p) => p.name === product.polygonName
-  )!;
+  const product = productData[0]; 
+  const destination = venue.locations.find(
+    (p) => p.name === product.location
+  )! as MappedinLocation;
   navigateTo(destination);
   searchElement.value = product.name;
 }
